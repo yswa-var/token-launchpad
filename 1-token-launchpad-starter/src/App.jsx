febb09476ed18fd76864ddbaf9017313fce7dbf0
yswa-var <yswa.var@gmail.com>
2025-07-16 01:02:35 +0530

    Integrate Solana wallet functionality and create token feature in the TokenLaunchpad. Updated Vite configuration to include node polyfills and modified App component to support wallet connection and UI elements.

--- 1-token-launchpad-starter/src/App.jsx
+++ 1-token-launchpad-starter/src/App.jsx
@@ -1,9 +1,36 @@
+import React, { FC, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+// Default styles that can be overridden by your app
+import '@solana/wallet-adapter-react-ui/styles.css';
 import './App.css'
 import { TokenLaunchpad } from './components/TokenLaunchpad'
 
 function App() {
   return (
-    <TokenLaunchpad></TokenLaunchpad>
+    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+            <WalletProvider wallets={[]} autoConnect>
+                <WalletModalProvider>
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        padding: '20px',
+                    }}>
+                        <WalletMultiButton />
+                        <WalletDisconnectButton />
+                    </div>
+                    <TokenLaunchpad />
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>  
   )
 }
 
--- 1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ 1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -1,5 +1,36 @@
+import { useWallet } from '@solana/wallet-adapter-react';
+import { useConnection } from '@solana/wallet-adapter-react';
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+import { getMinimumBalanceForRentExemptMint, createInitializeMint2Instruction, TOKEN_2022_PROGRAM_ID, MINT_SIZE } from "@solana/spl-token";
+
 
 export function TokenLaunchpad() {
+    const { connection } = useConnection();
+    const wallet = useWallet();
+
+    const createToken = async () => {
+        console.log('createToken');
+        const mintKeypair = Keypair.generate();
+        const lamports = await getMinimumBalanceForRentExemptMint(connection);
+
+        const transaction = new Transaction().add(
+            SystemProgram.createAccount({
+                fromPubkey: wallet.publicKey,
+                newAccountPubkey: mintKeypair.publicKey,
+                space: MINT_SIZE,
+                lamports,
+                programId: TOKEN_2022_PROGRAM_ID,
+            }),
+            createInitializeMint2Instruction(mintKeypair.publicKey, 9, wallet.publicKey, wallet.publicKey, TOKEN_2022_PROGRAM_ID)
+        );
+
+        transaction.feePayer = wallet.publicKey;
+        transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+        transaction.partialSign(mintKeypair);
+
+        await wallet.sendTransaction(transaction, connection);
+        console.log(`Token mint created at ${mintKeypair.publicKey.toBase58()}`);
+    }
     return  <div style={{
         height: '100vh',
         display: 'flex',
@@ -12,6 +43,6 @@ export function TokenLaunchpad() {
         <input className='inputText' type='text' placeholder='Symbol'></input> <br />
         <input className='inputText' type='text' placeholder='Image URL'></input> <br />
         <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
-        <button className='btn'>Create a token</button>
+        <button onClick={createToken} className='btn'>Create a token</button>
     </div>
 }
